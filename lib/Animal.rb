ca5ec3dcdf34158e701a4a335ea72ae680e0ce0b
Agandaur <cjwagstaffe@gmail.com>
2020-02-02 14:03:32 -0800

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,26 @@
 class Animal
 
+    attr_accessor :weight, :zoo
+    attr_reader :nickname, :species
+
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        save
+    end
+
+    def self.find_by_species(species)
+        @@all.select {|a| a.species == species}
+    end
+
+    def save
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,35 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+wpz = Zoo.new("Woodland Park Zoo", "WA")
+cmz = Zoo.new("Cougar Mountain Zoo", "WA")
+sbz = Zoo.new("Santa Barbara Zoo", "CA")
 
+cmz.new_animal("Cougar", 160, "Cuzco")
+cmz.new_animal("Cougar", 175, "Cassie")
+
+wpz.new_animal("Wolf", 130, "Waldo")
+wpz.new_animal("Wolf", 136, "Wendy")
+wpz.new_animal("Wolf", 141, "Wanda")
+wpz.new_animal("Moose", 1400, "Martin")
+wpz.new_animal("Moose", 750, "Molly")
+wpz.new_animal("Lion", 350, "Nalla")
+wpz.new_animal("Lion", 500, "Simba")
+wpz.new_animal("Crocodile", 500, "Carlos")
+wpz.new_animal("Tiger", 360, "Tilly")
+wpz.new_animal("Tiger", 446, "Thomas")
+
+sbz.new_animal("Otter", 23,"Otto")
+sbz.new_animal("Otter", 7, "Ozzy")
+sbz.new_animal("Otter", 7, "Olley")
+sbz.new_animal("Otter", 9, "Olive")
+sbz.new_animal("Lemur", 5, "Lizzy")
+sbz.new_animal("Lemur", 5, "Larz")
+sbz.new_animal("Common Raven", 3, "Poe")
+sbz.new_animal("Opssum", 6, "Rusty")
+sbz.new_animal("Giraffe", 1800, "Gemima")
+sbz.new_animal("Giraffe", 1800, "Gerald")
+sbz.new_animal("Giraffe", 1800, "Gary")
 
 binding.pry
 puts "done"
