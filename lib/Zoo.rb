32e2dd75a67976799ffc11f8c2f0c48c6656fe4f
github username <github email address>
2020-02-03 11:01:15 -0800

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,36 @@
 class Zoo
+  attr_reader :location, :name
+  @@all = []
 
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def animals
+    Animal.all.select { |animal| animal.zoo == self }
+  end
+
+  def animal_species
+    x = self.animals.map { |animal| animal.species }
+    x.uniq
+  end
+
+  def find_by_species(species)
+    self.animals.select { |animal| animal.species == self }
+  end
+
+  def animal_nicknames
+    x = self.animals.map { |animal| animal.nickname }
+    x.uniq
+  end
+
+  def self.find_by_location(location)
+    self.all.select { |zoo| zoo.location == location }
+  end
 end
--- run.rb
+++ run.rb
@@ -1,10 +1,16 @@
 require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
-require 'pry'
-
+require "pry"
 
 #Test your code here
+National = Zoo.new("National Zoo", "DC")
+Pondicherry = Zoo.new("Pondicherry", "India")
+San_Diego = Zoo.new("San Diego Zoo", "San Diego")
 
+Richard_Parker = Animal.new("tiger", 320, "Richdard Parker")
+Mother = Animal.new("gorilla", 240, "Mary")
+Apollo = Animal.new("gerbil", 3, "Apollo")
+Hades = Animal.new("gerbil", 1.4, "Hades")
 
 binding.pry
 puts "done"
