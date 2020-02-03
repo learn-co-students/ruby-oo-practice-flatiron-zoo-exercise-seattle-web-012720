e4df1f3d94f3bc64c9c5ac134df7737a0039ab4a
dianakw8591 <dianakw@gmail.com>
2020-02-03 12:03:42 -0800

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,33 @@
 class Zoo
+    @@all = []
+    def self.all
+        @@all
+    end
 
+    attr_reader :name, :location
+    def initialize(name, location)
+        @name = name
+        @location = location
+        self.class.all << self
+    end
+
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        animals.map {|animal| animal.species}.uniq
+    end
+
+    def find_by_species(species)
+        animals.select {|animals| animals.species == species}
+    end
+
+    def animal_nicknames
+        animals.map {|animal| animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        self.all.select {|zoo| zoo.location == location}
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,30 @@ require 'pry'
 
 
 #Test your code here
+g1 = Animal.new("giraffe", 500, "lanky")
+g2 = Animal.new("giraffe", 500, "spotty")
+g3 = Animal.new("giraffe", 500, "awks")
+g4 = Animal.new("giraffe", 500, "on-the-move")
 
+l1 = Animal.new("lion", 200, "nala")
+l2 = Animal.new("lion", 200, "simba")
+l3 = Animal.new("lion", 200, "mufasa")
+l4 = Animal.new("lion", 200, "scar")
+
+sd_zoo = Zoo.new("San Diego Zoo", "San Diego")
+chi_zoo = Zoo.new("Chicago Zoo", "Chicago")
+sea_zoo = Zoo.new("Seattle Zoo", "Seattle")
+sea_zoo2 = Zoo.new("Other Seattle Zoo", "Seattle")
+
+g1.captured_by_zoo(sd_zoo)
+g2.captured_by_zoo(sd_zoo)
+g3.captured_by_zoo(chi_zoo)
+g4.captured_by_zoo(sea_zoo)
+
+l1.captured_by_zoo(sea_zoo)
+l2.captured_by_zoo(sea_zoo2)
+l3.captured_by_zoo(sea_zoo2)
+l4.captured_by_zoo(chi_zoo)
 
 binding.pry
 puts "done"
