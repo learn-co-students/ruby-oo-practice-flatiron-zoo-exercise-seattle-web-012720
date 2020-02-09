7e29d2301a8d78cb371e186499192580dbfd66a5
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-02-09 12:06:56 -0800

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,50 @@
 class Zoo
 
+    attr_accessor :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all
+        self.all.each do |zoo|
+            zoo.name == self
+        end
+    end
+
+    def animals
+        Animal.all.select do |animal_object|
+            animal_object.zoo == self
+        end
+    end
+
+    def animal_species
+        self.all.map {|animal_object| animal_object.species}
+    end
+
+    def find_by_species(animal_species)
+        self.animals.select do |animal_object|
+            animal_object.species == species
+        end
+    end
+
+    def animal_nicknames
+        self.animals.map do |animal_object|
+            animal_object.nickname
+        end
+    end
+
+    def self.find_by_location(location)
+        location = Zoo.all.select{|zoo_object| zoo_object.location == lo}
+        location.map{|zoo_object| zoo_object.name}
+    
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,16 @@ require 'pry'
 
 
 #Test your code here
+ 
+z1 = Zoo.new("C1", "Uberlandia")
+z2 = Zoo.new("C2", "Uberaba")
+z3 = Zoo.new("C3", "Araguari")
 
+
+a1 = Animal.new("zebra", "200", "Zinha", "C1")
+a2 = Animal.new("cabra", "100", "Biba", "C2")
+a3 = Animal.new("egua", "300", "Pinha", "C3")
+a4 = Animal.new("giraffa", "500", "Pota", "C2")
 
 binding.pry
 puts "done"
